8773e22d18bb85f3c5f8757d3bc3f584eee51c7b
Shahid321fw11 <sidansari20@gmail.com>
2023-10-14 19:18:49 +0530

    design: header and footer design done

---
+++ client/src/routes/Footer.svelte
@@ -0,0 +1,367 @@
+<script>
+</script>
+
+<div>
+	<div class="justify-between align-center bg-primary-red-900 bg-black">
+		<div
+			class="mx-auto py-5 px-4 sm:px-6 lg:px-8 flex md:flex-row flex-col items-center justify-center border-b border-opacity-20 border-[rgba(209,213,219,0.2)]"
+		>
+			<img
+				src="https://masaischool.com/images/footer/arrow-gif.gif"
+				alt="arrow"
+				class="w-[96px] h-[96px] mr-[16px] hidden lg:block"
+				style=""
+			/>
+			<h2 class="font-extrabold tracking-tight text-gray-900">
+				<span class="block text-white text-[24px] font-semibold">Get Started</span>
+				<span class="block text-[#FFDB66] text-16 font-semibold">Join the Program</span>
+			</h2>
+			<button
+				class="inline-flex items-center mt-4 md:mt-0 md:ml-4 justify-center px-5 py-2.5 text-base font-medium rounded-md text-white bg-red-500"
+				>Get your dream job
+			</button>
+		</div>
+
+		<div class="max-w-[80rem] mx-auto">
+			<div
+				class="w-full justify-between py-5 px-2 sm:hidden lg:flex"
+				style="box-shadow: rgba(243, 242, 242, 0.2) 0px 1px 0px 0px;"
+			>
+				<a href="/"
+					><img src="https://masaischool.com/images/footer/masai-logo-dark.svg" alt="logo" /></a
+				>
+				<div class="flex items-center">
+					<p class="text-base leading-6 text-neutral-400 font-normal">Follow us -</p>
+					<a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://twitter.com/masaischool"
+						target="_blank"
+						rel="noreferrer"
+						><img src="https://masaischool.com/images/footer/twitter-logo.svg" alt="twitter" />
+					</a><a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://www.facebook.com/masaischool/"
+						target="_blank"
+						rel="noreferrer"
+						><img src="https://masaischool.com/images/footer/facebook-logo.svg" alt="fb" />
+					</a><a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://www.linkedin.com/company/masai-school/"
+						target="_blank"
+						rel="noreferrer"
+						><img src="https://masaischool.com/images/footer/linkedin-logo.svg" alt="linkedin" />
+					</a><a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://www.instagram.com/masaischool/"
+						target="_blank"
+						rel="noreferrer"
+						><img src="https://masaischool.com/images/footer/instagram-logo.svg" alt="insta" />
+					</a><a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://github.com/masai-school"
+						target="_blank"
+						rel="noreferrer"
+						><img src="	https://masaischool.com/images/footer/github-logo.svg" alt="github" />
+					</a><a
+						class="ml-8 brightness-[.6] hover:brightness-[1] transition-all"
+						href="https://www.youtube.com/channel/UCENOACKQiqejXP-bb9sCnMg?view_as=subscriber"
+						target="_blank"
+						rel="noreferrer"
+						><img src="https://masaischool.com/images/footer/youtube-logo.svg" alt="youtube" />
+					</a>
+				</div>
+			</div>
+			<div>
+				<div
+					class="sm:pt-6 lg:pt-0 p-[16px] lg:p-[64px_32px] lg:mt-10 grid pb-9 grid-cols-[100%] xl:grid-cols-[1fr_.5fr]"
+				>
+					<div>
+						<div class="flow-root mb-5">
+							<div class="-mt-4 -ml-8 flex flex-wrap lg:-ml-4">
+								<div class="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+									<a href="/"
+										><span
+											class="inline-flex items-center rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-gray-800 font-sans"
+											>Full Stack Web Development - Full Time</span
+										></a
+									>
+								</div>
+								<div class="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+									<a href="/"
+										><span
+											class="inline-flex items-center rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-gray-800 font-sans"
+											>Data Analytics - Full Time</span
+										></a
+									>
+								</div>
+								<div class="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+									<a href="/"
+										><span
+											class="inline-flex items-center rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-gray-800 font-sans"
+											>Mobile Development - Full Time</span
+										></a
+									>
+								</div>
+								<div class="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+									<a href="/"
+										><span
+											class="inline-flex items-center rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-gray-800 font-sans"
+											>Software Testing &amp; Automation - Full Time</span
+										></a
+									>
+								</div>
+								<div class="mt-4 ml-8 flex flex-shrink-0 flex-grow lg:ml-4 lg:flex-grow-0">
+									<a href="/"
+										><span
+											class="inline-flex items-center rounded-full bg-gray-100 px-4 py-2.5 text-sm font-semibold text-gray-800 font-sans"
+											>Backend Development - Full Time</span
+										></a
+									>
+								</div>
+							</div>
+						</div>
+						<div
+							class="flex flex-col sm:flex-wrap sm:flex-row space-y-5 sm:space-y-2 sm:space-x-4 mb-5"
+						/>
+						<div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+							<a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/success-stories"
+								target="_self"
+								rel="noreferrer"
+								><p>Success Stories</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/hire-masai-graduates"
+								target="_self"
+								rel="noreferrer"
+								><p>Hire From Us</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/about"
+								target="_self"
+								rel="noreferrer"
+								><p>About Us</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/teams"
+								target="_self"
+								rel="noreferrer"
+								><p>Our Team</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/our-investors"
+								target="_self"
+								rel="noreferrer"
+								><p>Our Investors</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/faq"
+								target="_self"
+								rel="noreferrer"
+								><p>FAQ</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/careers"
+								target="_self"
+								rel="noreferrer"
+								><p>Careers</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/media?page=1"
+								target="_self"
+								rel="noreferrer"
+								><p>Newsroom</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/referral"
+								target="_self"
+								rel="noreferrer"
+								><p>Referral Program</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="https://blog.masaischool.com/"
+								target="_black"
+								rel="noreferrer"
+								><p>Masai Blog</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/contact"
+								target="_self"
+								rel="noreferrer"
+								><p>Contact Us</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="https://masaischool.com/learn/?utm_source=10th_Oct_masai_main_footer&amp;utm_medium=ma[…]ain_footer_to_learn&amp;utm_campaign=10th_Oct_masai_main_footer"
+								target="_black"
+								rel="noreferrer"
+								><p>Masai Learn</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/talent-circle"
+								target="_self"
+								rel="noreferrer"
+								><p>Talent Circle</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/become-coach"
+								target="_self"
+								rel="noreferrer"
+								><p>Become Coach</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/industry-mentors"
+								target="_self"
+								rel="noreferrer"
+								><p>Industry Mentors</p>
+							</a><a
+								class="hover:text-primary-ms-red-500 w-fit text-neutral-200 font-normal text-base leading-6"
+								href="/msat"
+								target="_self"
+								rel="noreferrer"
+								><p>Masai School Admissions Test (MSAT)</p>
+							</a>
+						</div>
+					</div>
+					<div class="mx-auto mt-10 xl:mt-0 flex gap-[16px] flex-col items-center justify-center">
+						<p class="text-center text-[#CECCCD] leading-6 text-base font-normal">
+							Connect with a growing <br /> community of learners
+						</p>
+						<a
+							target="_blank"
+							href="https://masailms.openinapp.co/prepleaf"
+							class="tracking-[1.25px] leading-6 text-sm text-[#3470E4] mt-[.5rem] md:mt-0 text-center h-[48px] items-center justify-between uppercase font-semibold cursor-pointer hover:bg-[#d7d7d7] rounded-[10px] bg-[#FFF] py-2 px-4 align-center flex gap-[8px]"
+							><svg
+								width="32"
+								height="32"
+								viewBox="0 0 32 32"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+								><g clip-path="url(#clip0_27143_251903)"
+									><circle cx="16" cy="16" r="16" fill="url(#paint0_linear_27143_251903)" /><path
+										d="M23.9847 9.38147C24.1272 8.46093 23.2519 7.73435 22.4334 8.09371L6.13123 15.2512C5.54427 15.509 5.5872 16.398 6.19597 16.5919L9.55789 17.6625C10.1995 17.8668 10.8943 17.7612 11.4546 17.3741L19.0343 12.1375C19.2628 11.9795 19.512 12.3045 19.3167 12.5059L13.8607 18.131C13.3314 18.6767 13.4365 19.6013 14.0731 20.0005L20.1817 23.8312C20.8668 24.2608 21.7482 23.8292 21.8764 23.0013L23.9847 9.38147Z"
+										fill="white"
+									/></g
+								><defs
+									><linearGradient
+										id="paint0_linear_27143_251903"
+										x1="16"
+										y1="0"
+										x2="16"
+										y2="32"
+										gradientUnits="userSpaceOnUse"
+										><stop stop-color="#37BBFE" /><stop
+											offset="1"
+											stop-color="#007DBB"
+										/></linearGradient
+									><clipPath id="clip0_27143_251903"
+										><rect width="32" height="32" fill="white" /></clipPath
+									></defs
+								></svg
+							>
+
+							Join our Telegram</a
+						>
+					</div>
+				</div>
+			</div>
+			<div
+				class="w-full justify-center flex-wrap gap-[0.5rem_2rem] py-5 flex lg:hidden items-center px-[16px]"
+				style="box-shadow: rgba(151, 151, 151, 0.25) 0px -1px 0px, rgba(151, 151, 151, 0.25) 0px 1px 0px;"
+			>
+				<a
+					class="brightness-[.6]"
+					href="https://twitter.com/masaischool"
+					target="_blank"
+					rel="noreferrer"
+					><img src="https://masaischool.com/images/footer/twitter-logo.svg" alt="twitter" />
+				</a><a
+					class="brightness-[.6]"
+					href="https://www.facebook.com/masaischool/"
+					target="_blank"
+					rel="noreferrer"
+					><img src="https://masaischool.com/images/footer/facebook-logo.svg" alt="fb" />
+				</a><a
+					class="brightness-[.6]"
+					href="https://www.linkedin.com/company/masai-school/"
+					target="_blank"
+					rel="noreferrer"
+					><img src="https://masaischool.com/images/footer/linkedin-logo.svg" alt="linkedin" />
+				</a><a
+					class="brightness-[.6]"
+					href="https://www.instagram.com/masaischool/"
+					target="_blank"
+					rel="noreferrer"
+					><img src="	https://masaischool.com/images/footer/instagram-logo.svg" alt="insta" />
+				</a><a
+					class="brightness-[.6]"
+					href="https://github.com/masai-school"
+					target="_blank"
+					rel="noreferrer"
+					><img src="	https://masaischool.com/images/footer/github-logo.svg" alt="github" />
+				</a><a
+					class="brightness-[.6]"
+					href="https://www.youtube.com/channel/UCENOACKQiqejXP-bb9sCnMg?view_as=subscriber"
+					target="_blank"
+					rel="noreferrer"
+					><img src="	https://masaischool.com/images/footer/youtube-logo.svg" alt="youtube" />
+				</a>
+			</div>
+			<div
+				class="py-5 lg:px-3 px-4 md:px-0 flex justify-between items-center"
+				style="box-shadow: rgba(243, 242, 242, 0.2) 0px -1px 0px 0px;"
+			>
+				<p class="sm:hidden lg:block text-neutral-500 leading-[22px] font-normal text-base">
+					© 2023 by Masai School | A Nolan Edutech Pvt Ltd Venture
+				</p>
+				<p class="lg:hidden text-neutral-500 leading-4 font-normal text-[10px]">
+					© 2023 by Masai School
+				</p>
+				<div class="text-right">
+					<a
+						href="/privacy"
+						rel="noreferrer"
+						class="underline cursor-pointer text-neutral-500 sm:leading-4 lg:leading-[18px] sm:text-[10px] lg:text-xs inline font-normal"
+						>Privacy Policy</a
+					>
+					<span
+						class="text-neutral-500 sm:leading-4 lg:leading-[18px] sm:text-[10px] lg:text-xs font-normal inline mx-0"
+						>&amp;</span
+					>
+					<a
+						href="/terms"
+						rel="noreferrer"
+						class="underline cursor-pointer text-neutral-500 sm:leading-4 lg:leading-[18px] sm:text-[10px] lg:text-xs inline font-normal"
+						>Terms and Conditions</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div
+	class="sticky bottom-0 h-[fit-content] flex bg-[rgba(255,_255,_255,_0.87)] md:hidden items-center w-full z-[2]"
+	style="filter: drop-shadow(rgba(0, 0, 0, 0.1) 0px -8px 32px); backdrop-filter: blur(6px);"
+>
+	<div
+		class="relative z-2 p-[16px] flex justify-evenly items-center h-full w-full flex-wrap gap-[7px]"
+	>
+		<div
+			class="!font-[600] text-[14px] leading-[24px] font-sans transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 flex-1 max-w-[30%] !text-[#544D4F]"
+		>
+			Get Started
+			<br />
+			<p
+				class="!font-[400] text-[12px] leading-[16px] font-sans transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 !text-[#544D4F]"
+			>
+				Join the Program
+			</p>
+		</div>
+		<button
+			type="button"
+			class="active:!ring-0 bg-red-500 hover:bg-red-600 rounded-[8px] text-white !font-[600] text-[14px] leading-[24px] font-sans tracking-[1.25px] uppercase p-[12px_16px] inline-block focus:!ring-0 outline-offset-[5px] outline-[1px] outline-[transparent] focus-visible:!shadow-[0_0_0_3px_rgba(66,_153,_225,_0.6)] disabled:opacity-[0.6] disabled:cursor-not-allowed transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 flex-1 text-center !px-0"
+			>Get your dream job</button
+		>
+	</div>
+</div>
