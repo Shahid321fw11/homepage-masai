6ecdb94aa838718217c01d10fcdc82afd434a011
Shahid321fw11 <sidansari20@gmail.com>
2023-10-15 11:41:52 +0530

    design: added hero component

---
+++ client/src/components/Hero.svelte
@@ -0,0 +1,12 @@
+<div class="w-screen">
+	<img
+		class="md:hidden w-screen"
+		src="https://masai-website-images.s3.ap-south-1.amazonaws.com/banner_m_1_b63dd244db.webp"
+		loading="lazy"
+	/>
+	<img
+		class="hidden md:block w-screen"
+		src="https://masai-website-images.s3.ap-south-1.amazonaws.com/banner_h_1_3d43d26bd0.webp"
+		loading="lazy"
+	/>
+</div>
