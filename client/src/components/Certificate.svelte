abb32439af11a5b8c0c67ad473c089369454067a
Shahid321fw11 <sidansari20@gmail.com>
2023-10-15 11:37:00 +0530

    design: added component of certification by NSDC

---
+++ client/src/components/Certificate.svelte
@@ -0,0 +1,53 @@
+<div class="mt-[60px]">
+	<div
+		class="p-[16px] md:rounded-[8px] overflow-hidden bg-[#F2F6FF] max-w-[1280px] mx-auto grid md:grid-cols-[50%_1fr] gap-10"
+	>
+		<div class="p-[0px]">
+			<h3
+				class="font-[700] text-[24px] leading-[32px] font-poppins transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 mb-[20px] text-center md:hidden !text-[#0A0103]"
+			>
+				<span class="!text-[#ED0331] relative whitespace-nowrap isolate"
+					>Certification
+					<img
+						loading="lazy"
+						alt="yellow-vector"
+						class="mx-auto absolute w-full md:w-[264px] bottom-[-5px] md:bottom-[-5px] z-[10] h-[14px] md:h-[21px] left-0 right-0"
+						style="opacity: 1; transition: opacity 1200ms ease-out 0s;"
+						src="https://masaischool.com/images/new-homepage/yellow-vector.svg"
+					/>
+				</span> By The N.S.D.C
+			</h3>
+			<img
+				src="https://masaischool.com/images/new-homepage/nsdc/nsdc-cert.jpg"
+				class="object-cover rounded-[8px] border-[0.568823px] border-[#0087D1]"
+				style=""
+				alt=""
+			/>
+		</div>
+		<div class="lg:pt-[40px] bg-[#F2F6FF]">
+			<h2
+				class="font-[700] text-[34px] leading-[40px] font-poppins transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 hidden md:block !text-[#0A0103]"
+			>
+				<span class="!text-[#ED0331] relative isolate"
+					>Certification
+					<img
+						loading="lazy"
+						alt="yellow-vector"
+						class="mx-auto absolute w-full md:w-[264px] bottom-[-8px] md:bottom-[-8px] z-[10] h-[14px] md:h-[21px] left-0 right-0"
+						style="opacity: 1; transition: opacity 1200ms ease-out 0s;"
+						src="https://masaischool.com/images/new-homepage/yellow-vector.svg"
+					/>
+				</span> By The N.S.D.C
+			</h2>
+			<p
+				class="!font-[400] text-[16px] leading-[24px] font-sans transition-[background-color,border-color,color,fill,stroke,opacity,box-shadow,transform] duration-200 text-center md:text-left mt-[-24px] md:mt-[24px] !text-[#21191B]"
+			>
+				NSDC Certification is a prestigious recognition for students who have completed accredited
+				skill-based training programs. It validates their expertise, demonstrating their dedication
+				to practical skills. This certification provides a competitive edge in the job market,
+				showcasing proficiency and adherence to industry standards. NSDC certifications are widely
+				accepted, unlocking rewarding career opportunities and personal growth.
+			</p>
+		</div>
+	</div>
+</div>
