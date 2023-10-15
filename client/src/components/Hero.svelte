f0c6e56af10a0e71c0d1d916a82debb3e61db1d1
Shahid321fw11 <sidansari20@gmail.com>
2023-10-15 11:44:34 +0530

    design: hero section suggetion of alt removed

--- client/src/components/Hero.svelte
+++ client/src/components/Hero.svelte
@@ -1,9 +1,11 @@
 <div class="w-screen">
+	<!-- svelte-ignore a11y-missing-attribute -->
 	<img
 		class="md:hidden w-screen"
 		src="https://masai-website-images.s3.ap-south-1.amazonaws.com/banner_m_1_b63dd244db.webp"
 		loading="lazy"
 	/>
+	<!-- svelte-ignore a11y-missing-attribute -->
 	<img
 		class="hidden md:block w-screen"
 		src="https://masai-website-images.s3.ap-south-1.amazonaws.com/banner_h_1_3d43d26bd0.webp"
